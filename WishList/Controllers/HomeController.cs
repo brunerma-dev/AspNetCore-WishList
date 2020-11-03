d62811531b303653a8e767bbbfba0b10c4f7f243
Mike Bruner <brunerma@icloud.com>
2020-11-03 11:15:31 -0800

    Completed module

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
